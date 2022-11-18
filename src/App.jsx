508ba41ed7425e2658f32daf2ff1ff801d9712b0
Lorentz <91217473+Lauro235@users.noreply.github.com>
2022-11-18 10:19:33 +0000

    add vite env variable plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import.meta.env
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  console.log(import.meta.env.VITE_SECRET)
+  
   return (
     <div className="App">
       <div>
