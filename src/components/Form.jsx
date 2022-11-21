aa19c96edd3e6393df929abc21d00e0652970007
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-11-21 17:08:06 +0000

    form submitting data object and console logging it correctly

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,54 +1,144 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Form() {
-  return (
-    <div>
-      {/* Form Heading */}
+    const [form, setForm] = useState({
+        name: '',
+        firstLine: '',
+        secondLine: '',
+        town: '',
+        postcode: '',
+        phone: '',
+        email: '',
+        website: '',
+    });
 
-      {/*
-      Info about them
-        name
-        adress
-          Number
-          Street Name 
-          Town/City
-          Post Code *
-        telephone
-        email address
-      */}
-      <form className='form'>
-        <label className='name-label' htmlFor="name">Name</label>
-        <input className='name-input' type="text" id="name" name="name" placeholder='Type company name'/>
-        <fieldset className='address'>
-          <legend className='address-heading' htmlFor="address">Your Business Address:</legend>
+    function handleChange(e) {
+        let value = e.target.value;
+        let name = e.target.name;
+        setForm({ ...form, [name]: value });
+    }
 
-          <label className='firstline-label' htmlFor="first-line">Number or name of property</label>
-          <input className='firstline-input' type="text" id="first-line" name="first-line" placeholder='Number or name of property' required/>
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(form);
+    }
 
-          <label className='secondline-label' htmlFor="second-line">Street Name</label>
-          <input className='secondline-input' type="text" id="second-line" name="second-line" placeholder='Street name'/>
+    return (
+        <div>
+            <form className='form' onSubmit={handleSubmit}>
+                <label className='name-label' htmlFor='name'>
+                    Name
+                </label>
+                <input
+                    onChange={handleChange}
+                    value={form.name}
+                    className='name-input'
+                    type='text'
+                    id='name'
+                    name='name'
+                    placeholder='Type company name'
+                />
+                <fieldset className='address'>
+                    <legend className='address-heading' htmlFor='address'>
+                        Your Business Address:
+                    </legend>
 
-          <label className='thirdline-label' htmlFor="third-line">Town / City</label>
-          <input className='secondline-input' type="text" id="third-line" name="third-line" placeholder='Town / City'/>
+                    <label className='firstline-label' htmlFor='first-line'>
+                        Number or name of property
+                    </label>
+                    <input
+                        onChange={handleChange}
+                        value={form.firstLine}
+                        className='firstline-input'
+                        type='text'
+                        id='first-line'
+                        name='firstLine'
+                        placeholder='Number or name of property'
+                        required
+                    />
 
-          <label className='thirdline-label' htmlFor="third-line">Post Code</label>
-          <input className='secondline-input' type="text" id="third-line" name="third-line" placeholder='Post code' required/>
-          
-        </fieldset>
+                    <label className='secondline-label' htmlFor='second-line'>
+                        Street Name
+                    </label>
+                    <input
+                        onChange={handleChange}
+                        value={form.secondLine}
+                        className='secondline-input'
+                        type='text'
+                        id='second-line'
+                        name='secondLine'
+                        placeholder='Street name'
+                    />
 
-        <label className='phone-label' htmlFor="phone">Phone</label>
-        <input className='phone-input' type="tel" id="phone" name="phone" placeholder='Type company phone'/>
+                    <label className='town-label' htmlFor='town-line'>
+                        Town / City
+                    </label>
+                    <input
+                        onChange={handleChange}
+                        value={form.town}
+                        className='town-input'
+                        type='text'
+                        id='town-line'
+                        name='town'
+                        placeholder='Town / City'
+                    />
 
-        <label className='email-label' htmlFor="email">Email</label>
-        <input className='email-input' type="email" id="email" name="email" placeholder='Type company email'/>
+                    <label className='postcode-label' htmlFor='postcode-line'>
+                        Post Code
+                    </label>
+                    <input
+                        onChange={handleChange}
+                        value={form.postcode}
+                        className='postcode-input'
+                        type='text'
+                        id='postcode-line'
+                        name='postcode'
+                        placeholder='Post code'
+                        required
+                    />
+                </fieldset>
 
-        <label className='website-label' htmlFor="website">Company Website</label>
-        <input className='website-input' type="url" id="website" name="website" placeholder='Your company website'/>
-        
-      </form>
+                <label className='phone-label' htmlFor='phone'>
+                    Phone
+                </label>
+                <input
+                    onChange={handleChange}
+                    value={form.phone}
+                    className='phone-input'
+                    type='tel'
+                    id='phone'
+                    name='phone'
+                    placeholder='Type company phone'
+                />
 
+                <label className='email-label' htmlFor='email'>
+                    Email
+                </label>
+                <input
+                    onChange={handleChange}
+                    value={form.email}
+                    className='email-input'
+                    type='email'
+                    id='email'
+                    name='email'
+                    placeholder='Type company email'
+                />
 
-    </div>
+                <label className='website-label' htmlFor='website'>
+                    Company Website
+                </label>
+                <input
+                    onChange={handleChange}
+                    value={form.website}
+                    className='website-input'
+                    type='url'
+                    id='website'
+                    name='website'
+                    placeholder='Your company website'
+                />
 
-  )
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    );
 }
