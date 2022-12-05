eafe93f269b1175291749f0f3f1fc511084a5235
St0neofFr33dom <christophefreeston@googlemail.com>
2022-12-05 16:16:38 +0000

    changed background colour of infobox and text colour to match figma. added focus transition of box to change size. Displays box and search bar ontop of map

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./InfoBox.css";
 function InfoBox({ props }) {
   return (
-    <div className="InfoBox">
+    <div className="InfoBox" tabIndex={0}>
   
         <div className="values">
-          <h4>{props.name}</h4>
+          <h4 style={{fontWeight:"bold"}}>{props.name}</h4>
           <h4>{props.firstLine} {props.secondLine}</h4>
           <h4>{props.town}</h4>
           <h4>{props.postcode}</h4>
