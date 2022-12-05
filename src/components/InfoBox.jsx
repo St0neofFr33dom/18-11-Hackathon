f46c01c2130ee9ae72095b1cd4e96bf7631b95a5
Lorentz <91217473+Lauro235@users.noreply.github.com>
2022-12-05 15:53:50 +0000

    small changes to info box

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -3,23 +3,13 @@ import "./InfoBox.css";
 function InfoBox({ props }) {
   return (
     <div className="InfoBox">
-      <h2>{props.name}</h2>
-      <div className="data">
-        <div className="labels">
-          <h4>Address:</h4>
-          <h4>Town and Postcode</h4>
-          <h4>Telephone:</h4>
-          <h4>E-Mail:</h4>
-          <h4>Website:</h4>
-        </div>
+  
         <div className="values">
+          <h4>{props.name}</h4>
           <h4>{props.firstLine} {props.secondLine}</h4>
-          <h4>{props.town}, {props.postcode}</h4>
-          <h4>{props.phone}</h4>
-          <h4>{props.email}</h4>
-          <h4>{props.website}</h4>
+          <h4>{props.town}</h4>
+          <h4>{props.postcode}</h4>
         </div>
-      </div>
     </div>
   );
 }
