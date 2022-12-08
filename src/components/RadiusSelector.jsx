e80d4492745f268aa1fa42b9994ddd0e72ff35d9
St0neofFr33dom <christophefreeston@googlemail.com>
2022-12-08 16:44:09 +0000

    fills the radius progress with green on the left side of the thumb. changed class names of input to global class, adds event listeners to the input on the map.jsx file

--- src/components/RadiusSelector.jsx
+++ src/components/RadiusSelector.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import styles from "./RadiusSelector.module.css"
+import React from "react";
+import styles from "./RadiusSelector.module.css";
 
-function RadiusSelector  ({radius, handleChange}) {
-    return (
-        <div className={styles.radiusSelector}>
-        <h4 className={styles.label}>Mile Radius</h4>
-                <input id={styles.slider1} type='range' value={radius} max={25} step={0.5} onChange={handleChange} className={styles.slider} />
-                <h4 className={styles.label}>Hello World</h4>
-        </div>
-    )
+function RadiusSelector({ radius, handleChange }) {
+  return (
+    <div className={styles.radiusSelector}>
+      <h4 className={styles.label}>Mile Radius</h4>
+      <input
+        id={styles.slider1}
+        type="range"
+        value={radius}
+        max={25}
+        step={0.5}
+        onChange={handleChange}
+        className="slider slider-progress"
+      />
+    </div>
+  );
 }
 
-export default RadiusSelector
+export default RadiusSelector;
