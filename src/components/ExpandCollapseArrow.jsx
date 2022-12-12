cbcc26d0af4e3434c2a6e3871f9a81ae0ac865f9
St0neofFr33dom <christophefreeston@googlemail.com>
2022-12-12 13:14:02 +0000

    made components for the collapse arrow and for text with icons, made the Infobox preview a display grid, made global font-size 14px

---
+++ src/components/ExpandCollapseArrow.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import arrow from '../assets/arrows.svg';
+import styles from './ExpandCollapseArrow.module.css'
+
+function ExpandCollapseArrow ({state, setState}) {
+    return (
+        <div className={styles.container}>
+        {state && <p>hide</p>}
+            <img
+                className={`${styles.arrow} ${state ? `${styles.rotateArrow}` : ''}`}
+                src={arrow}
+                alt='arrow'
+                onClick={() => {
+                    setState(!state);
+                }}
+            />
+            {!state && <p>details</p>}
+        </div>
+    )
+}
+
+export default ExpandCollapseArrow
--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -1,42 +1,41 @@
 import { useState } from 'react';
 import './InfoBox.css';
-import arrow from '../assets/arrows.svg';
+import ExpandCollapseArrow from './ExpandCollapseArrow';
+import TextWithIcon from './TextWithIcon';
+import {faEnvelope, faPhone, faGlobe } from '@fortawesome/free-solid-svg-icons';
+
+
 function InfoBox({ props }) {
     const [expand, setExpand] = useState(false);
     return (
         <div className={`InfoBox ${expand ? `expandedInfoBox` : ''}`}>
-            <img
-                className={`arrow ${expand ? `rotateArrow` : ''}`}
-                src={arrow}
-                alt='arrow'
-                onClick={() => {
-                    setExpand(!expand);
-                }}
-            />
-            <p>{`${expand ? 'hide' : 'details'}`}</p>
-            <div className='values'>
-                <h4 style={{ fontWeight: 'bold' }}>{props.name}</h4>
-                <h4>
-                    {props.firstLine} {props.secondLine}
-                </h4>
-                <h4>{props.town}</h4>
-                <h4>{props.postcode}</h4>
+            <div className='preview'>
+                <div className='values'>
+                    <h4 style={{ fontWeight: 'bold' }}>{props.name}</h4>
+                    <h4>
+                        {props.firstLine} {props.secondLine}
+                    </h4>
+                    <h4>{props.town}</h4>
+                    <h4>{props.postcode}</h4>
+                </div>
+                <ExpandCollapseArrow state={expand} setState={setExpand} />
+                <div></div>
             </div>
             <div className='contact-details'>
-                <h4>{props.phone}</h4>
-                <h4>{props.email}</h4>
-                <h4>{props.website}</h4>
+            <TextWithIcon text={props.phone} icon={faPhone} />
+            <TextWithIcon text={props.email} icon={faEnvelope}/>
+            <TextWithIcon text={props.website} icon={faGlobe}/>
             </div>
             <div className='survey'>
                 <h4>have you visited {props.name}?</h4>
                 <h4>check all that apply</h4>
                 <br />
-                <h4>Opening Hours</h4>
+                <h4>opening hours</h4>
                 <h4>staff are friendly</h4>
                 <h4>easy to find</h4>
                 <h4>parking is available</h4>
                 <h4>produce is good quality</h4>
-                <h4>whellchair access available</h4>
+                <h4>wheelchair access available</h4>
             </div>
         </div>
     );
