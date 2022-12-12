4a58b483e8dea51874efa906f5cd23a191b24c59
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-12-12 16:35:39 +0000

    transition of infoBox content fades in and out with slide in

--- src/components/InfoBox/index.jsx
+++ src/components/InfoBox/index.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react';
 import './InfoBox.css';
 import ExpandCollapseArrow from '../ExpandCollapseArrow';
 import TextWithIcon from '../TextWithIcon';
-import {faEnvelope, faPhone, faGlobe } from '@fortawesome/free-solid-svg-icons';
-
+import {
+    faEnvelope,
+    faPhone,
+    faGlobe,
+} from '@fortawesome/free-solid-svg-icons';
 
 function InfoBox({ props }) {
     const [expand, setExpand] = useState(false);
@@ -21,21 +24,24 @@ function InfoBox({ props }) {
                 <ExpandCollapseArrow state={expand} setState={setExpand} />
                 <div></div>
             </div>
-            <div className='contact-details'>
-            <TextWithIcon text={props.phone} icon={faPhone} />
-            <TextWithIcon text={props.email} icon={faEnvelope}/>
-            <TextWithIcon text={props.website} icon={faGlobe}/>
-            </div>
-            <div className='survey'>
-                <h4>have you visited {props.name}?</h4>
-                <h4>check all that apply</h4>
-                <br />
-                <h4>opening hours</h4>
-                <h4>staff are friendly</h4>
-                <h4>easy to find</h4>
-                <h4>parking is available</h4>
-                <h4>produce is good quality</h4>
-                <h4>wheelchair access available</h4>
+
+            <div className={`hiddenText ${expand ? `expandedText` : ''}`}>
+                <div className='contact-details'>
+                    <TextWithIcon text={props.phone} icon={faPhone} />
+                    <TextWithIcon text={props.email} icon={faEnvelope} />
+                    <TextWithIcon text={props.website} icon={faGlobe} />
+                </div>
+                <div className='survey'>
+                    <h4>have you visited {props.name}?</h4>
+                    <h4>check all that apply</h4>
+                    <br />
+                    <h4>opening hours</h4>
+                    <h4>staff are friendly</h4>
+                    <h4>easy to find</h4>
+                    <h4>parking is available</h4>
+                    <h4>produce is good quality</h4>
+                    <h4>wheelchair access available</h4>
+                </div>
             </div>
         </div>
     );
