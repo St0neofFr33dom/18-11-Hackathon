35fce8808515fc3948c857792311660223686cef
michaelrushton-dev <michaelwjrushton@gmail.com>
2023-01-20 15:29:21 +0000

    Auth0 set up. rendering login and logout buttons on MapPage and it all works.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <Auth0Provider
+            domain='dev-zne4nad02tfmlvun.eu.auth0.com'
+            clientId='LZ3Sj1CkbTphsLt01zyWX4QgXUZmLoTw'
+            authorizationParams={{
+                redirect_uri: window.location.origin,
+            }}
+        >
+            <App />
+        </Auth0Provider>
+    </React.StrictMode>
+);
--- src/pages/MapPage/index.jsx
+++ src/pages/MapPage/index.jsx
@@ -9,6 +9,8 @@ import { dummyData } from '../../data/dummdata.js';
 import getGeolocation from '../../funcs/getGeolocation';
 import styles from './MapPage.module.css';
 import browserWidthContext from '../../context/browserWidthContext';
+import LoginButton from '../../components/LoginButton';
+import LogoutButton from '../../components/LogoutButton';
 
 function MapPage({ foodBanks }) {
     const [radius, setRadius] = useState(10);
@@ -37,48 +39,52 @@ function MapPage({ foodBanks }) {
     }
 
     return (
-        <div
-            className={` ${styles.mapContainer} ${
-                desktop ? styles.desktopMapContainer : ''
-            }`}
-        >
-            <Wrapper apiKey={import.meta.env.VITE_SECRET} render={render}>
-                <Map
-                    searchCoordinates={searchCoordinates}
-                    setSearchCoordinates={setSearchCoordinates}
-                    radius={radius}
-                    setRadius={setRadius}
-                >
-                    {/* can only map through foodBanks after async fetch so checks if it exists before trying to map using && */}
-                    {foodBanks &&
-                        foodBanks.map((place, index) => {
-                            return (
-                                <Marker
-                                    key={index}
-                                    position={{
-                                        lat: place.lat,
-                                        lng: place.lng,
-                                    }}
-                                    data={place}
-                                    setState={() => {
-                                        setDisplayedData(place);
-                                    }}
-                                />
-                            );
-                        })}
-                    <MileRadius
-                        center={searchCoordinates}
-                        radius={radius * 1000}
-                        fillColor='#0F0'
-                        cursor={null}
-                    />
-                </Map>
-            </Wrapper>
-            {/* <SearchBar setSearchCoordinates={setSearchCoordinates} /> */}
-            {foodBanks && <InfoBox props={displayedData} />}
+        <>
+            <LoginButton />
+            <LogoutButton />
+            <div
+                className={` ${styles.mapContainer} ${
+                    desktop ? styles.desktopMapContainer : ''
+                }`}
+            >
+                <Wrapper apiKey={import.meta.env.VITE_SECRET} render={render}>
+                    <Map
+                        searchCoordinates={searchCoordinates}
+                        setSearchCoordinates={setSearchCoordinates}
+                        radius={radius}
+                        setRadius={setRadius}
+                    >
+                        {/* can only map through foodBanks after async fetch so checks if it exists before trying to map using && */}
+                        {foodBanks &&
+                            foodBanks.map((place, index) => {
+                                return (
+                                    <Marker
+                                        key={index}
+                                        position={{
+                                            lat: place.lat,
+                                            lng: place.lng,
+                                        }}
+                                        data={place}
+                                        setState={() => {
+                                            setDisplayedData(place);
+                                        }}
+                                    />
+                                );
+                            })}
+                        <MileRadius
+                            center={searchCoordinates}
+                            radius={radius * 1000}
+                            fillColor='#0F0'
+                            cursor={null}
+                        />
+                    </Map>
+                </Wrapper>
+                {/* <SearchBar setSearchCoordinates={setSearchCoordinates} /> */}
+                {foodBanks && <InfoBox props={displayedData} />}
 
-            {/* <Form locations={locations} setLocations={setLocations} /> */}
-        </div>
+                {/* <Form locations={locations} setLocations={setLocations} /> */}
+            </div>
+        </>
     );
 }
 
